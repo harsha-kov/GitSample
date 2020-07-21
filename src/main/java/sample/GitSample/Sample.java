a8e35e5ecbb1ea98241cc2db3e9ccdc1d216c044
harsha <harsha.kov@gmail.com>
2020-07-21 13:41:24 +0530

    commit done

---
+++ src/main/java/sample/GitSample/Sample.java
@@ -0,0 +1,11 @@
+package sample.GitSample;
+
+public class Sample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Sample Program");
+
+	}
+
+}
